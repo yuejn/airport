97efbd03e92488f9156a0060d8d3986dcf97454e
yuejn <git@yuejn.com>
2015-01-23 15:06:26 +0800

    updated tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -69,6 +69,6 @@ class TicketsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def ticket_params
-      params.require(:ticket).permit(:flight_no, :passenger_no)
+      params.require(:ticket).permit(:flight_id, :passenger_id)
     end
 end
