70b445044f70676ee41912b191e091ecdcc5ff34
yuejn <git@yuejn.com>
2015-01-26 14:31:22 +0800

    added departure_date to views and controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -69,6 +69,6 @@ class FlightsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def flight_params
-      params.require(:flight).permit(:destination)
+      params.require(:flight).permit(:destination, :departure_date)
     end
 end
